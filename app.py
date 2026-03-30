e4bfe0c04ee463c205923d49659ff32fe37e9b23
khag-0808 <agrawalkhushi0804@gmail.com>
2026-03-30 21:04:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request
 from database import init_db, save_meeting, is_slot_available
-import traceback
 
 app = Flask(__name__)
 
 # Init DB
 try:
     init_db()
-    print("DB OK")
 except Exception as e:
     print("DB ERROR:", e)
 
@@ -20,32 +18,26 @@ def home():
 @app.route('/book', methods=['POST'])
 def book():
     try:
-        print("STEP 1")
-
         name = request.form.get('name')
         email = request.form.get('email')
         date = request.form.get('date')
         time = request.form.get('time')
 
-        print("STEP 2", name, email)
-
         if not is_slot_available(date, time):
             return "Slot already booked"
 
-        print("STEP 3")
-
         meet_link = "https://meet.google.com/test-link"
-
-        print("STEP 4")
 
         save_meeting(name, email, date, time, meet_link)
 
-        print("STEP 5 SAVED")
-
-        return "SUCCESS DATABASE WORKING"
+        return render_template("success.html",
+                               name=name,
+                               date=date,
+                               time=time,
+                               meet_link=meet_link)
 
     except Exception as e:
-        return f"<pre>{traceback.format_exc()}</pre>"
+        return f"ERROR: {str(e)}"
 
 
 if __name__ == "__main__":
