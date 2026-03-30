4777d1202b1670c5fbbd355bf7a314f021bda830
khag-0808 <agrawalkhushi0804@gmail.com>
2026-03-30 22:52:36 +0530

    Update email_service.py

--- email_service.py
+++ email_service.py
@@ -1,47 +1,28 @@
-import smtplib
-import ssl
 import os
-from email.message import EmailMessage
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 def send_confirmation_email(receiver_email, name, date, time, meet_link):
     try:
-        SENDER_EMAIL = os.getenv("SENDER_EMAIL")
-        APP_PASSWORD = os.getenv("APP_PASSWORD")
+        sg = SendGridAPIClient(os.getenv("SENDGRID_API_KEY"))
 
-        if not SENDER_EMAIL or not APP_PASSWORD:
-            print("Email credentials missing")
-            return
-
-        subject = "Meeting Confirmation | Akshar Paaul"
-
-        html_content = f"""
-        <html>
-        <body>
+        message = Mail(
+            from_email='agrawalkhushi0804@gmail.com',
+            to_emails=receiver_email,
+            subject='Meeting Confirmation | Akshar Paaul',
+            html_content=f"""
             <h2>Meeting Confirmed</h2>
             <p>Hello {name},</p>
             <p>Your meeting is scheduled.</p>
             <p><b>Date:</b> {date}</p>
             <p><b>Time:</b> {time}</p>
             <p><a href="{meet_link}">Join Meeting</a></p>
-        </body>
-        </html>
-        """
+            """
+        )
 
-        msg = EmailMessage()
-        msg["From"] = SENDER_EMAIL
-        msg["To"] = receiver_email
-        msg["Subject"] = subject
-        msg.set_content("Your email client does not support HTML")
-        msg.add_alternative(html_content, subtype="html")
-
-        context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            server.login(SENDER_EMAIL, APP_PASSWORD)
-            server.send_message(msg)
-
+        sg.send(message)
         print("Email sent successfully")
 
     except Exception as e:
-        print("EMAIL FAILED:", e)
+        print("SendGrid Error:", e)
